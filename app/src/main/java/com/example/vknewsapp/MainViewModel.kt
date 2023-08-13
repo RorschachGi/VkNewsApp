497e287d8b0f66a6474a3646f2928d3b75c83edc
RorschachGi <tumanovAccount7773@rambler.ru>
2023-08-13 11:10:50 +0400

    add Jetpack Compose Navigation

--- app/src/main/java/com/example/vknewsapp/MainViewModel.kt
+++ app/src/main/java/com/example/vknewsapp/MainViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.vknewsapp.domain.FeedPost
 import com.example.vknewsapp.domain.StatisticItem
+import com.example.vknewsapp.ui.theme.NavigationItem
 
 class MainViewModel: ViewModel() {
 
@@ -18,7 +19,13 @@ class MainViewModel: ViewModel() {
     val feedPosts: LiveData<List<FeedPost>>
         get() = _feedPosts
 
+    private val _selectedNavItem = MutableLiveData<NavigationItem>(NavigationItem.Home)
+    val selectedNavItem: LiveData<NavigationItem>
+        get() = _selectedNavItem
 
+    fun selectNavItem(item: NavigationItem){
+        _selectedNavItem.value = item
+    }
 
     fun updateCount(feedPost: FeedPost, item: StatisticItem){
         val oldPosts = feedPosts.value?.toMutableList() ?: mutableListOf()
--- app/src/main/java/com/example/vknewsapp/ui/theme/VkNewsMainScreen.kt
+++ app/src/main/java/com/example/vknewsapp/ui/theme/VkNewsMainScreen.kt
@@ -1,41 +1,43 @@
 package com.example.vknewsapp.ui.theme
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.clickable
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.runtime.livedata.observeAsState
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
 import com.example.vknewsapp.MainViewModel
+import com.example.vknewsapp.navigation.AppNavGraph
+import com.example.vknewsapp.navigation.NavigationState
+import com.example.vknewsapp.navigation.Screen
+import com.example.vknewsapp.navigation.rememberNavigationState
 
 
-@OptIn(ExperimentalMaterialApi::class, ExperimentalFoundationApi::class)
 @Composable
 fun MainScreen(viewModel: MainViewModel){
+    val navigationState = rememberNavigationState()
     Scaffold(
         bottomBar = {
             BottomNavigation{
-                val selectedItemPosition = remember {
-                    mutableStateOf(0)
-                }
+                val navBackStackEntry by navigationState.navHostController.currentBackStackEntryAsState()
+                val currentRoute = navBackStackEntry?.destination?.route
+
                 val items = listOf(
                     NavigationItem.Home,
                     NavigationItem.Favourite,
                     NavigationItem.Profile
                 )
-                items.forEachIndexed{index, item ->
+                items.forEach { item ->
                     BottomNavigationItem(
-                        selected = selectedItemPosition.value == index,
-                        onClick = { selectedItemPosition.value = index },
-                        icon = {Icon(item.icon, contentDescription = null)},
+                        selected = currentRoute == item.screen.route,
+                        onClick = {
+                                  navigationState.navigateTo(item.screen.route)
+                        },
+                        icon = { Icon(item.icon, contentDescription = null) },
                         label = {
                             Text(text = stringResource(id = item.titleResId))
                         },
@@ -45,50 +47,23 @@ fun MainScreen(viewModel: MainViewModel){
                 }
             }
         }
-    ) {
-        val feedPosts = viewModel.feedPosts.observeAsState(listOf())
-        LazyColumn(
-            modifier = Modifier.padding(it),
-            contentPadding = PaddingValues(
-                top = 16.dp,
-                start = 8.dp,
-                end = 8.dp,
-                bottom = 20.dp
-            ),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(
-                items = feedPosts.value,
-                key = { it.id }
-            ) { feedPost ->
-                val dismissState = rememberDismissState()
-                if(dismissState.isDismissed(DismissDirection.EndToStart)){
-                    viewModel.remove(feedPost)
-                }
-                SwipeToDismiss(
-                    modifier = Modifier.animateItemPlacement(),
-                    state = dismissState,
-                    background = {},
-                    directions = setOf(DismissDirection.EndToStart)
-                ) {
-                    PostCard(
-                        feedPost = feedPost,
-                        onCommentClickListener = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onLikeClickListener = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onShareClickListener = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        },
-                        onViewsClickListener = { statisticItem ->
-                            viewModel.updateCount(feedPost, statisticItem)
-                        }
-                    )
-                }
-            }
-        }
-
+    ) {paddingValues ->
+        AppNavGraph(
+            navHostController = navigationState.navHostController,
+            homeScreenContent = { HomeScreen(viewModel = viewModel, paddingValues = paddingValues) },
+            favouriteScreenContent = { TextCounter(name = "Favourite") },
+            profileScreenContent = {TextCounter(name = "Profile") }
+        )
     }
+}
+
+@Composable
+private fun TextCounter(name: String){
+    var count by rememberSaveable {
+        mutableStateOf(0)
+    }
+    Text(
+        modifier = Modifier.clickable {count++},
+        text = "$name Count: $count",
+        color = Color.Black)
 }
